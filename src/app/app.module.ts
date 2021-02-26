2292d1ec493b2ee02ee6cb0d53f3b2ddb5eac1d4
vuyyuruarvr <vuyyuruarvr@gmail.com>
2021-02-26 13:15:43 +0530

    assignment_update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,3 @@
-// import { HttpClientModule } from '@angular/common/http';
-// import { NgModule } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
-// import { MatCardModule } from '@angular/material/card';
-// import { MatExpansionModule, MatExpansionPanel } from '@angular/material/expansion';
-// import { MatFormFieldModule } from '@angular/material/form-field';
-// import { MatToolbarModule } from '@angular/material/toolbar';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { AppComponent } from './app.component';
-// import { HeaderComponent } from './header/header.component';
-// import { NotesService } from './notes.service';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     HeaderComponent
-//   ],
-//   imports: [
-//     MatCardModule,
-//     // MatDividerModule,
-//     MatExpansionModule,
-//     // MatListModule,
-//     // MatStepperModule,
-//     // MatTabsModule,
-//     MatFormFieldModule,
-//     // MatInputModule,
-//     // MatButtonModule,
-//     BrowserModule,
-//     BrowserAnimationsModule,
-//     MatToolbarModule,
-//     FormsModule,
-//     HttpClientModule
-    
-//   ],
-
-//   providers: [
-//     NotesService
-//   ],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
-
-
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
@@ -54,11 +8,7 @@ import { HttpClientModule } from '@angular/common/http'
 import { NotesService } from './notes.service';
 import { HeaderComponent } from './header/header.component';
 import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatListModule } from '@angular/material/list';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatTabsModule } from '@angular/material/tabs';
 import { MatFormFieldModule, MatInputModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
